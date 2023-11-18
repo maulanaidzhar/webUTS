33f02da1b39b2d4d483c167d280792d467d4fd45
maulanaidzhar <144698669+maulanaidzhar@users.noreply.github.com>
2023-11-19 02:36:37 +0700

    uts commit

--- js/main.js
+++ js/main.js
@@ -21,3 +21,18 @@ document.querySelector('#bell-icon').onclick = () =>{
     bell.classList.toggle('active');
 }
 
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
